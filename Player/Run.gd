47e13b668d51d7f6975e37add6e82badbf18fb7c
dashedstripes <adam@pateo.co>
2020-05-25 14:56:43 -0500

    0.0.1: fix bug where sprite flipped for one frame before state change to idle

--- Player/Run.gd
+++ Player/Run.gd
@@ -22,6 +22,9 @@ func physics_process(delta):
 	var input_velocity = Vector2.ZERO
 	input_velocity.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	
+	if(owner.velocity == Vector2.ZERO && input_velocity == Vector2.ZERO):
+		exit("Idle")
+	
 	if(input_velocity.x > 0):
 		owner.direction = Vector2.RIGHT
 	elif(input_velocity.x < 0):
@@ -31,9 +34,6 @@ func physics_process(delta):
 	
 	if(Input.is_action_just_pressed("jump") && owner.is_on_floor()):
 		exit("Jump")
-	
-	if(owner.velocity == Vector2.ZERO && input_velocity == Vector2.ZERO):
-		exit("Idle")
 
 	if(input_velocity != Vector2.ZERO):
 		owner.velocity = owner.velocity.move_toward(input_velocity * owner.speed, delta * owner.acceleration)
